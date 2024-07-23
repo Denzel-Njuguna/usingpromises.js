53654d91993d7287035e613e9202210bff344e19
Denzel <144164130+Denzel-Njuguna@users.noreply.github.com>
2024-07-23 15:48:33 +0300

    Update-promises.js

--- promises.js
+++ promises.js
@@ -24,48 +24,10 @@ order(2000,(()=>console.log(`${ingredients.fruits[0]} was ordered`)))
 
 
 .then(()=>{
-return order(2000,()=>{let ingredients = {
-  fruits:["banana","oranges","apples","mango"]
-};
-let flavors = ingredients.fruits.map((name)=>{
-  return name;
-});
-let order=(fruit_name)=>{
-  
-  console.log("your order has been received\n" + flavors.join('\n'));
-  setTimeout(()=>{
-    console.log(ingredients.fruits[fruit_name], " was selected");
-  },2000)
-  
-  let production=()=>{
-    setTimeout(()=>{
-      console.log("your ice cream is being processed ");
-    },2000)
-  
-    
-    let serving =()=>{
-      setTimeout(()=>{
-        console.log("here is your order");
-      },2000)
-      
-      
-      let paying = ()=>{
-        setTimeout(()=>{
-          console.log("your bill is 12.99");
-        },2000)
-        
-      
-      };
-    paying();
-    };
-  serving();
-  };
-production();
-};
-
-order(2);
-  console.log("the fruit was chopped")
-})})
+return order(2000,()=>{
+  console.log("the fruit was chopped");
+  })
+})
 
 .then(()=>{
   return order(2000,()=>{
